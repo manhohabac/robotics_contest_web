ea8dac8f9ceef31043ae76cb1be8adc6206521b8
Tran Cong Minh <minhtcppdev@gmail.com>
2024-11-10 21:58:11 +0700

    Re-deploy 1

--- robot_web/settings.py
+++ robot_web/settings.py
@@ -80,17 +80,17 @@ WSGI_APPLICATION = 'robot_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Cấu hình kết nối đến database từ biến môi trường `DATABASE_URL`
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+# }
 
 
 # Password validation
