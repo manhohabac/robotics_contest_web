47907fd849d6ccbdeee3e4f67c858b26ed750f40
Tran Cong Minh <minhtcppdev@gmail.com>
2024-11-10 21:50:57 +0700

    Update build.sh 2

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -o igncr  # Ignore carriage return
+
+# Đảm bảo các kí tự dòng mới là Unix-style (LF)
+dos2unix requirements.txt  # Nếu có lệnh dos2unix, hãy chạy nó trên file requirements.txt
 
 # Cài đặt các phụ thuộc từ requirements.txt
 pip install -r requirements.txt
